76a0afbeed452e4f1d5d8b8042fb98a4a1601487
MikBar2501 <45071328+MikBar2501@users.noreply.github.com>
2024-01-24 19:47:13 +0100

    Lekcja 2 i 3
    Wprowadzenie kontrolera gracza oraz kamer. Dodanie terenów oraz interakcji z nimi. Reakcja na teren poprzez zmianę prędkości.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 12f;
+    Vector3 velocity;
+    CharacterController characterController;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z; 
+        characterController.Move(move * speed * Time.deltaTime);
+
+        RaycastHit hit;
+        if(Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out hit, 0.4f, groundMask))
+        {
+            string terrainType;
+            terrainType = hit.collider.gameObject.tag;
+            switch (terrainType)
+            {
+                default:
+                    speed = 12;
+                    break;
+                case "Bad":
+                    speed = 3;
+                    break;
+                case "Good":
+                    speed = 20;
+                    break;
+            }
+                
+        }
+
+    }
+}
